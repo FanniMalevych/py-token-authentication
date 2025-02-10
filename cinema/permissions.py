09f8b2758ad2e2d5a1f5a83edc7318deb34a34c5
fanni <fmalevych@zoolatech.com>
2025-02-10 13:52:17 +0200

    add custom permission

---
+++ cinema/permissions.py
@@ -0,0 +1,17 @@
+from rest_framework.permissions import BasePermission, SAFE_METHODS
+
+
+class IsAdminAllOrIsAuthenticatedReadOnly(BasePermission):
+    """
+    The request for admin user to get/update/delete data
+    For auth user only to get data
+    """
+
+    def has_permission(self, request, view):
+        return bool(
+            request.method in SAFE_METHODS and
+            request.user and
+            request.user.is_authenticated
+        ) or (
+                request.user and request.user.is_staff
+        )
